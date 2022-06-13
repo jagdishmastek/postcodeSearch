b5d8b3fcfd8785e3f013b50f80023e0b035fb520
Jagdish Gadhe <Jagdish10668@Mastek.com>
2022-06-13 13:42:49 +0530

    Updates

---
+++ precache-manifest.97321d5dac114e65dce15674e8666fbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "22312354bad95a7ddbb4",
+    "url": "/postcodesearch/static/css/main.8ff37a05.chunk.css"
+  },
+  {
+    "revision": "22312354bad95a7ddbb4",
+    "url": "/postcodesearch/static/js/main.5280f404.chunk.js"
+  },
+  {
+    "revision": "b979822541bb506175bc",
+    "url": "/postcodesearch/static/js/runtime~main.d3a2d2cd.js"
+  },
+  {
+    "revision": "d15772ef87ef5343c91c",
+    "url": "/postcodesearch/static/css/2.e44f5781.chunk.css"
+  },
+  {
+    "revision": "d15772ef87ef5343c91c",
+    "url": "/postcodesearch/static/js/2.76d7c7b8.chunk.js"
+  },
+  {
+    "revision": "21855e7efadd33218164",
+    "url": "/postcodesearch/static/js/3.a1be9f81.chunk.js"
+  },
+  {
+    "revision": "8d165ca9026472dd778b6fbb37ede9e0",
+    "url": "/postcodesearch/static/media/mountain.8d165ca9.jpg"
+  },
+  {
+    "revision": "985182962f82893ffd4335fabbe03792",
+    "url": "/postcodesearch/index.html"
+  }
+];
